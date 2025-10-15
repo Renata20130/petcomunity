bc6e0249fd3b745c4ab55d72ca729d2831a013d7
Renata González <renatagonzalez2021@gmail.com>
2025-10-15 15:28:39 -0300

    Guardando cambios recientes antes de subir a GitHub Pages

--- petcomunity/settings.py
+++ petcomunity/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 import os
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
