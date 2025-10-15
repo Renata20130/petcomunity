32bda6b01d5b88ac627913385b113c03ba2a3557
Renata González <renatagonzalez2021@gmail.com>
2025-10-15 18:10:17 -0300

    Fix locale setting for Render deployment

--- petcomunity/settings.py
+++ petcomunity/settings.py
@@ -133,9 +133,9 @@ USE_L10N = True
 USE_TZ = True
 
 try:
-    locale.setlocale(locale.LC_ALL, 'C.UTF-8')  # Compatible con Render
+    locale.setlocale(locale.LC_ALL, 'es_CL.UTF-8')
 except locale.Error:
-    pass
+    locale.setlocale(locale.LC_ALL, '') 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
